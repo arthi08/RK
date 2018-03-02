19cc5c7c44297bc3ea625ecf74578cea91967b46
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-03-02 15:57:11 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int b;
+    scanf("%d",&b);
+    if(b%7==0)
+    {
+        printf("mulitiples of 7 \n");
+        
+    }
+    else
+    {
+        printf("not a multiples of 7 \n");
+    }
+}
